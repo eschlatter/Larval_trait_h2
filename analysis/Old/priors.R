72ad1f681b837fb1514fc34528d4c927678ab074
eschlatter <eschlatter1@gmail.com>
2022-10-10 20:51:17 -0400

    File to generate all manuscript and supplement figures is complete. Moved files around and tidied up.

---
+++ analysis/Old/priors.R
@@ -0,0 +1,16 @@
+#parameter-expanded prior
+prior_expand_params <- list(V = 1,nu = 1000, alpha.mu=0, alpha.V=1)
+
+#also parameter-expanded, from tuto_en; with residual edited (nu=0.002 instead of nu=1) to be proper
+prior_ext_params <- list(V=1,nu=1,alpha.mu=0,alpha.V=1000)
+
+#inverse gamma prior
+prior_invgamma_params <- list(V = 1,nu = .002)
+
+#gentler inverse gamma prior
+prior_invgamma2_params <- list(V = 1,nu = .02)
+
+#flat prior
+prior_flat_params <- list(V = 1e-10,nu = -1) #trusting based on course notes that this is actually flat, because I can't tell what a negative nu means
+
+
--- functions/model_fns.R
+++ functions/model_fns.R
@@ -475,3 +475,115 @@ plot_priorandpost_two <- function(model,prior,xlim=0){
     
   }
 }
+
+priorandpost_plot <- function(model,prior,xlim=0,i,xax=FALSE){
+  
+  xvals = seq(0,xlim,length.out=nrow(model$VCV))  
+  param_list = colnames(model$VCV)
+  
+  #get prior parameters for the current variance component
+  if(i==length(param_list)) {prior_params = prior$R #last one is the residual
+  } else prior_params = prior$G[[i]] #others are elements of G
+  
+  #generate prior density (y values to match the existing xvals vector)
+  if(length(prior_params)==4){ #parameter-expanded priors
+    prior_density = df(xvals/prior_params$alpha.V, df1 = 1, df2 = prior_params$nu, ncp = (prior_params$alpha.mu^2)/prior_params$alpha.V)
+  } else if(length(prior_params)==2){ #regular inverse-gamma priors
+    prior_density = dinvgamma(xvals,shape = prior_params$nu/2,scale = prior_params$nu*prior_params$V/2)
+  } else print('Warning: cannot handle this type of prior')
+  
+  #get prior mode to use for plotting    
+  prior_mode = xvals[which.max(prior_density)]
+  if(prior_mode==xvals[length(xvals)]) print('Warning: xlim is too small')
+  
+  #grab posterior samples and convert to density
+  post_samples = model$VCV[,i]
+  post_density = density(post_samples)
+  #post_density$y = post_density$y/post_density$n
+  post_mode = post_density$x[which.max(post_density$y)]
+  
+  #do some scaling if necessary
+  multfactor=1
+  if((max(prior_density,na.rm=TRUE)*10) < max(post_density$y)){ #if the prior density is on a much smaller scale than the posterior
+    multfactor = 0.4*(max(post_density$y)/max(prior_density,na.rm=TRUE))
+    prior_density = prior_density * multfactor
+  }
+  
+  colors=c('Prior'='blue','Posterior'='red')
+  
+  if(xax==FALSE){
+    plottt <- ggplot(as.data.frame(model$VCV), aes(x=as.data.frame(model$VCV)[,i])) + 
+      theme_light()+
+      geom_density(aes(color='Posterior'))+
+      xlim(0,xlim)+
+      geom_line(aes(x = xvals, 
+                    y = prior_density,
+                    color='Prior'))+
+      labs(color='Legend')+
+      scale_color_manual(values=colors)+
+      scale_y_continuous(sec.axis = sec_axis(~./multfactor)) +
+      theme(axis.text.y.right=element_text(color='blue',margin=margin(l=-1,r=-1,unit='pt')),
+            axis.text.y.left=element_text(color='red',margin=margin(l=-1,r=-1,unit='pt')),
+            axis.text.y = element_text(size = 5),
+            axis.ticks = element_blank(),
+            axis.title.y = element_blank(),
+            axis.text.x=element_blank(),
+            legend.position = 'none')+
+      xlab(NULL)+
+      ylab(NULL)}
+  
+  if(xax==TRUE){
+    plottt <- ggplot(as.data.frame(model$VCV), aes(x=as.data.frame(model$VCV)[,i])) + 
+      theme_light()+
+      geom_density(aes(color='Posterior'))+
+      xlim(0,xlim)+
+      geom_line(aes(x = xvals, 
+                    y = prior_density,
+                    color='Prior'))+
+      labs(color='Legend')+
+      scale_color_manual(values=colors)+
+      scale_y_continuous(sec.axis = sec_axis(~./multfactor)) +
+      theme(axis.text.y.right=element_text(color='blue',margin=margin(l=-1,r=-1,unit='pt')),
+            axis.text.y.left=element_text(color='red',margin=margin(l=-1,r=-1,unit='pt')),
+            axis.text.y = element_text(size = 5),
+            axis.ticks = element_blank(),
+            axis.title.y = element_blank(),
+            axis.text.x=element_text(size=6),
+            legend.position = 'none')+
+      xlab(NULL)+
+      ylab(NULL)}
+  
+  return(plottt)
+}
+
+herit_plot <- function(model,prior,xax=FALSE){
+  herit = model$VCV[,1]/rowSums(as.data.frame(model$VCV))
+  
+  if(xax==FALSE){
+    plottt <- ggplot(as.data.frame(herit), aes(x=as.data.frame(herit)[,1])) + 
+      theme_light()+
+      geom_density()+
+      xlim(0,1)+
+      theme(axis.text.y=element_text(color='black',size=5,margin=margin(l=-1,r=-1,unit='pt')),
+            axis.ticks = element_blank(),
+            axis.title.y = element_blank(),
+            axis.text.x=element_blank(),
+            legend.position = 'none')+
+      xlab(NULL)+
+      ylab(NULL)}
+  
+  if(xax==TRUE){
+    plottt <- ggplot(as.data.frame(herit), aes(x=as.data.frame(herit)[,1])) + 
+      theme_light()+
+      geom_density()+
+      xlim(0,1)+
+      theme(axis.text.y=element_text(color='black',size=5,margin=margin(l=-1,r=-1,unit='pt')),
+            axis.ticks = element_blank(),
+            axis.title.y = element_blank(),
+            axis.text.x=element_text(size=6),
+            legend.position = 'none')+
+      xlab(NULL)+
+      ylab(NULL)}
+  
+  return(plottt)
+}
